7c844ebf63a217a771bfc39c2ae7116d9b96fb08
Lavibuu La <121961129+lavibula@users.noreply.github.com>
2023-05-10 01:25:51 +0700

    update Lab02

---
+++ src/Lab02/src/Aims.java
@@ -0,0 +1,23 @@
+
+public class Aims {
+
+	public static void main(String[] args) {
+		// create new Cart
+		Cart anOrder = new Cart();
+		
+		//create new DVD object and add them to the Cart
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		anOrder.addDigitalVideoDisc(dvd1);
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science iction", "George Lucas", 87, 24.95f);
+		anOrder.addDigitalVideoDisc(dvd2);
+		
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+		anOrder.addDigitalVideoDisc(dvd3);
+		
+		//print total cost
+		System.out.println("Total cost is: ");
+		System.out.println(anOrder.totalCost());
+	}
+
+}
---
+++ src/Lab02/src/Cart.java
@@ -0,0 +1,43 @@
+
+public class Cart {
+	// Add a field as an array to store a list of DigitalVideoDisc.  
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED]; 
+	
+	// To keep track of how many DigitalVideoDiscs are in the cart
+	private int qtyOrdered = 0;
+	
+	//add an item to the list
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+			itemsOdered[qtyOrdered++] = disc;
+			System.out.println("The disc has been added");
+		}
+		else {
+			System.out.println("The cart is almost full");
+		}
+		
+	}
+	
+	//remove the item passed by argument from the list
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		for(int i = 0; i <qtyOrdered; i++) {
+			if (itemsOdered[i] == disc) {
+				for(int j = i; j < qtyOrdered; j++) {
+					itemsOdered[j] = itemsOdered[i+1];
+				}
+				itemsOdered[--qtyOrdered] = null; 
+				
+			}
+		}
+	}
+	
+	//TotalCost method
+	public float totalCost() {
+		float totalCost = 0;
+		for (int i = 0; i < qtyOrdered; i++) {
+			totalCost +=  itemsOdered[i].getCost();
+		}
+		return totalCost;
+	}
+}
